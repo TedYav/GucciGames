cf5d237b64435afb76e58db90f8ad7ac4c2e2853
Mike Ma <ym67@duke.edu>
2015-11-11 20:48:45 -0500

    Created GAE framework, lol

---
+++ src/voogasalad_GucciGames/gameAuthoring/gui/map/GUIMapObject.java
@@ -0,0 +1,8 @@
+package voogasalad_GucciGames.gameAuthoring.gui.map;
+
+import javafx.scene.image.ImageView;
+
+public class GUIMapObject {
+	private ImageView myImageView;
+
+}
---
+++ src/voogasalad_GucciGames/gameAuthoring/gui/sidebar/ITab.java
@@ -0,0 +1,9 @@
+package voogasalad_GucciGames.gameAuthoring.gui.sidebar;
+
+public interface ITab{
+	
+	
+	public void update();
+	
+
+}
