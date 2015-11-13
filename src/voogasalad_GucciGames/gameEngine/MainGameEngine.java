cae2ff24c2b71243803da9bb65e91d7c3ee76c60
teddywhy <davidteoman@gmail.com>
2015-11-12 23:31:24 -0500

    added comment to game engine reflecting our discussion

--- src/voogasalad_GucciGames/gameEngine/MainGameEngine.java
+++ src/voogasalad_GucciGames/gameEngine/MainGameEngine.java
@@ -6,6 +6,16 @@ import voogasalad_GucciGames.gameEngine.gamePlayer.AllPlayers;
 import voogasalad_GucciGames.gameEngine.gameRule.GlobalGameRule;
 import voogasalad_GucciGames.gameEngine.gameRule.Goal.Goal;
 
+// TODO: 
+// *Store allowable tiles, units here -> UnitContainer, TileContainer
+// *Rename AllPlayers to PlayerContainer
+// *Allow for multiple maps -> MapContainer
+// *Add Name to Engine -> so we know what game this is...
+// *Decide if we want to serialize this class or create another just for the data
+// 		*Note, this may be nice, but kind of pointless, since this IS the game... :)
+// SEE THIS IMAGE: https://www.dropbox.com/sc/ouekq7n22pcm7uh/AAA35yvEEaprH9PkuDL5unk6a
+
+
 public class MainGameEngine {
 
 	private AllPlayers myGamePlayers;
