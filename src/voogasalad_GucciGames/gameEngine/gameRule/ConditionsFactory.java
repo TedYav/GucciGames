915814b8d524dc3137fd0254b9a49e907f3eac7f
sallymerza <sma45@duke.edu>
2015-11-15 09:59:17 -0500

    created command factory

--- src/voogasalad_GucciGames/gameEngine/gameRule/ConditionsFactory.java
+++ src/voogasalad_GucciGames/gameEngine/gameRule/ConditionsFactory.java
@@ -1,5 +1,7 @@
 package voogasalad_GucciGames.gameEngine.gameRule;
 
+import java.util.ResourceBundle;
+
 import voogasalad_GucciGames.gameEngine.gamePlayer.AllPlayers;
 import voogasalad_GucciGames.gameEngine.gameRule.oucomes.Outcome;
 
@@ -9,12 +11,19 @@ import voogasalad_GucciGames.gameEngine.gameRule.oucomes.Outcome;
  *
  */
 public class ConditionsFactory {
-	AllPlayers myPlayers;
-	Outcome myOutcomes;
+	private AllPlayers myPlayers;
+	private Outcome myOutcomes;
+	private final String CONDITIONS_PROPERTIES = "resources/conditions";
+	private final String OUTCOMES_PROPERTIES = "resources/outcomes";
+	private ResourceBundle conditionBudle;
+	private ResourceBundle outcomeBundle;
 
 	public ConditionsFactory(AllPlayers players) {
 		myPlayers = players;
 		myOutcomes = new Outcome(players);
+		conditionBudle = ResourceBundle.getBundle(CONDITIONS_PROPERTIES);
+		outcomeBundle = ResourceBundle.getBundle(OUTCOMES_PROPERTIES);
 	}
+	public void createCOndition(){}
 
 }
