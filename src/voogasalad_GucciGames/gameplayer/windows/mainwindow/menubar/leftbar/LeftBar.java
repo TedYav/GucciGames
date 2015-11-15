8d6a7e6970d4be4c7f66b842a86b5aba8ff72114
John Dai <johnrundai@gmail.com>
2015-11-15 02:01:32 -0500

    refactored maingamescene and leftbar

--- src/voogasalad_GucciGames/gameplayer/windows/mainwindow/menubar/leftbar/LeftBar.java
+++ src/voogasalad_GucciGames/gameplayer/windows/mainwindow/menubar/leftbar/LeftBar.java
@@ -1,5 +1,6 @@
 package voogasalad_GucciGames.gameplayer.windows.mainwindow.menubar.leftbar;
 
+import java.util.ResourceBundle;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.layout.VBox;
@@ -10,14 +11,17 @@ import voogasalad_GucciGames.gameplayer.windows.WindowComponent;
 public class LeftBar extends WindowComponent implements MiniDisplayComponent{
     private VBox container;
     private double spacing = 5;
-    public LeftBar (GameScene scene, GameEngineInterface game) {
+    ResourceBundle myBundle;
+    public LeftBar (GameScene scene, GameEngineInterface game, ResourceBundle bundle) {
         super(scene, game);
         container = new VBox(spacing);
+        myBundle=bundle;
         initializeData();
     }
     private void initializeData() {
         DisplayMapObjectDetails objectDetails = new DisplayMapObjectDetails();
         container.getChildren().add(objectDetails.getNodeToDraw());
+        container.getStyleClass().add(myBundle.getString("LeftVBox"));
     }
 
     @Override
