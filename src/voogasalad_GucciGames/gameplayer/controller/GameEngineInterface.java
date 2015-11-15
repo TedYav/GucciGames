5349e2e855c1209321226422fabbcbb87a80b9e7
John Dai <johnrundai@gmail.com>
2015-11-14 19:15:11 -0500

    updated engine interface

--- src/voogasalad_GucciGames/gameplayer/controller/GameEngineInterface.java
+++ src/voogasalad_GucciGames/gameplayer/controller/GameEngineInterface.java
@@ -3,4 +3,7 @@ package voogasalad_GucciGames.gameplayer.controller;
 // TODO: integrate this with GameEngineToActualGame
 public interface GameEngineInterface {
 
+    public String getName ();
+    public performAction(MapObject unit, );
+    
 }
