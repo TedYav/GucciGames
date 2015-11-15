07f894572383785a1a491c443eaac9c96af932ad
John Dai <johnrundai@gmail.com>
2015-11-15 14:57:39 -0500

    added some methods to playermapobjectinterface

--- src/voogasalad_GucciGames/gameplayer/windows/mainwindow/map/cell/contents/PlayerMapObjectInterface.java
+++ src/voogasalad_GucciGames/gameplayer/windows/mainwindow/map/cell/contents/PlayerMapObjectInterface.java
@@ -1,5 +1,8 @@
 package voogasalad_GucciGames.gameplayer.windows.mainwindow.map.cell.contents;
 
-public interface PlayerMapObjectInterface {
+import java.util.List;
 
+public interface PlayerMapObjectInterface {
+    public List<String> getAttributes(); //i.e. HP=100, Owning Player=1, ...
+    public String getImageUrl();
 }
