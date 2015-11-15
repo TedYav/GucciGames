418bf99c989bf23ab7b200177837c35bec438280
sallymerza <sma45@duke.edu>
2015-11-15 12:59:44 -0500

    created condition factory.. will be testing it

--- src/voogasalad_GucciGames/gameEngine/gameRule/Conditions.java
+++ src/voogasalad_GucciGames/gameEngine/gameRule/Conditions.java
@@ -1,6 +1,8 @@
 
 package voogasalad_GucciGames.gameEngine.gameRule;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import voogasalad_GucciGames.gameEngine.gamePlayer.GamePlayerPerson;
@@ -12,8 +14,23 @@ import voogasalad_GucciGames.gameEngine.gameRule.oucomes.Outcome;
  *
  */
 public abstract class Conditions {
+	private static List<Conditions> RulesInEffect;
+
 	public Conditions(List<GamePlayerPerson> players, Outcome outcome) {
+		RulesInEffect = new ArrayList<Conditions>();
 	}
+
+	public void addRule(Conditions ruleName) {
+		RulesInEffect.add(ruleName);
+	}
+
+	public void applyRules() {
+		Iterator<Conditions> ruleIterator = RulesInEffect.iterator();
+		while (ruleIterator.hasNext()) {
+			ruleIterator.next().execute();
+		}
+	}
+
 	public abstract void execute();
 
 }
--- src/voogasalad_GucciGames/gameEngine/gameRule/ConditionsFactory.java
+++ src/voogasalad_GucciGames/gameEngine/gameRule/ConditionsFactory.java
@@ -1,13 +1,19 @@
 package voogasalad_GucciGames.gameEngine.gameRule;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import voogasalad_GucciGames.gameEngine.gamePlayer.AllPlayers;
+import voogasalad_GucciGames.gameEngine.gamePlayer.GamePlayerPerson;
 import voogasalad_GucciGames.gameEngine.gameRule.oucomes.Outcome;
 
 /**
  *
- * @author Sally Al
+ * @author Sally Al will re-factor this class after testing
  *
  */
 public class ConditionsFactory {
@@ -17,13 +23,45 @@ public class ConditionsFactory {
 	private final String OUTCOMES_PROPERTIES = "resources/outcomes";
 	private ResourceBundle conditionBudle;
 	private ResourceBundle outcomeBundle;
+	private HackyCondRes conditionMap;
 
 	public ConditionsFactory(AllPlayers players) {
 		myPlayers = players;
 		myOutcomes = new Outcome(players);
 		conditionBudle = ResourceBundle.getBundle(CONDITIONS_PROPERTIES);
 		outcomeBundle = ResourceBundle.getBundle(OUTCOMES_PROPERTIES);
+		conditionMap = new HackyCondRes();
 	}
-	public void createCOndition(){}
+
+	public void createCondition(String name, String type, List<?> args)
+			throws InstantiationException, IllegalAccessException, ClassNotFoundException, IllegalArgumentException,
+			InvocationTargetException, NoSuchMethodException, SecurityException {
+		// 1. default vs custom rule
+		if (conditionMap.containsKey(name)) {
+			List<GamePlayerPerson> players = new ArrayList<GamePlayerPerson>();
+
+			if (type.equals("player")) {
+				if (args != null) {
+					List<Integer> playerID = (List<Integer>) args.get(0);
+					Iterator<Integer> idIterator = playerID.iterator();
+					while (idIterator.hasNext()) {
+						players.add(myPlayers.getActivePlayer(idIterator.next()));
+					}
+				}
+				// thanks Efe!
+				Class<Conditions> condition = (Class<Conditions>) Class.forName(name);
+				Constructor<Conditions> condConstructor = condition.getConstructor(players.getClass(),
+						myOutcomes.getClass());
+				Conditions conditionInstance = condConstructor.newInstance(players, myOutcomes);
+				conditionInstance.addRule(conditionInstance);
+			} else {
+				// add rules for levels
+			}
+		} else {
+			// add custom rules
+			// another if then so re-factor above into methods
+		}
+
+	}
 
 }
