167e76f957ecad9060bb45ea74b777af475b00a7
teddywhy <davidteoman@gmail.com>
2015-11-16 22:56:28 -0500

    fixed resolution issue

--- src/voogasalad_GucciGames/gameplayer/windows/GameWindow.java
+++ src/voogasalad_GucciGames/gameplayer/windows/GameWindow.java
@@ -3,7 +3,9 @@ package voogasalad_GucciGames.gameplayer.windows;
 import java.util.ResourceBundle;
 import java.util.stream.Collectors;
 
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.util.Map;
@@ -22,8 +24,17 @@ public abstract class GameWindow implements GameWindowInterface {
 
 	private void displayWindow(){
 		Map<String,String> myWindowConfig = parseConfig();
-		myStage.setWidth(Integer.parseInt(myWindowConfig.get("Width")));
-		myStage.setHeight(Integer.parseInt(myWindowConfig.get("Height")));
+		if(myWindowConfig.get("Width") ==null || myWindowConfig.get("Height") == null){
+			Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
+			myStage.setX(primaryScreenBounds.getMinX());
+			myStage.setY(primaryScreenBounds.getMinY());
+			myStage.setWidth(primaryScreenBounds.getWidth());
+			myStage.setHeight(primaryScreenBounds.getHeight());
+		}
+		else{
+			myStage.setWidth(Integer.parseInt(myWindowConfig.get("Width")));
+			myStage.setHeight(Integer.parseInt(myWindowConfig.get("Height")));
+		}
 		myStage.setResizable(Integer.parseInt(myWindowConfig.get("Resizable"))==1);
 		myStage.setTitle(myWindowConfig.get("Title"));
 		myStage.show();
