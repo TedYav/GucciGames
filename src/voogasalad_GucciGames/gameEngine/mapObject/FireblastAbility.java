a5c077ac0b5c528419293f243cf366ebdc5642a5
Joy Patel <joypatel0123@gmail.com>
2015-11-17 01:12:22 -0500

    Refactored moving actions to events

--- src/voogasalad_GucciGames/gameEngine/mapObject/FireblastAbility.java
+++ src/voogasalad_GucciGames/gameEngine/mapObject/FireblastAbility.java
@@ -5,14 +5,21 @@ import java.util.List;
 import voogasalad_GucciGames.gameEngine.GameMap;
 import voogasalad_GucciGames.gameEngine.CommunicationParams.CommunicationParams;
 import voogasalad_GucciGames.gameEngine.gamePlayer.AllPlayers;
+import voogasalad_GucciGames.gameEngine.objectActions.MapObjectEvent;
 import voogasalad_GucciGames.gameEngine.targetCoordinate.ATargetCoordinate;
 
-public class FireblastAbility implements IMapObjectAction{
+public class FireblastAbility extends MapObjectEvent{
+
+	public FireblastAbility(String actionName) {
+		super(actionName);
+		// TODO Auto-generated constructor stub
+	}
 
 	@Override
-	public CommunicationParams action(CommunicationParams communication, ATargetCoordinate targetCoords) {
+	protected CommunicationParams execute(CommunicationParams params) {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
 	
 }
