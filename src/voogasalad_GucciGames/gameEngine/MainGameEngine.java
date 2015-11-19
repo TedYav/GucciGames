f97ee8d5e4efdfbe44629a4e1c7b4c06a1edd30c
Efe Aras <efe.aras@duke.edu>
2015-11-18 22:56:39 -0500

    Created the connection between GameEngine and GamePlayer for moving around a single unit

--- src/voogasalad_GucciGames/gameEngine/MainGameEngine.java
+++ src/voogasalad_GucciGames/gameEngine/MainGameEngine.java
@@ -8,6 +8,7 @@ import java.util.List;
 import voogasalad_GucciGames.gameEngine.CommunicationParams.ActionToGamePlayerParameters;
 import voogasalad_GucciGames.gameEngine.CommunicationParams.BasicParameters;
 import voogasalad_GucciGames.gameEngine.CommunicationParams.GridCoordinateParameters;
+import voogasalad_GucciGames.gameEngine.CommunicationParams.LocationParams;
 import voogasalad_GucciGames.gameEngine.CommunicationParams.MainGameEngineCommunicationParameters;
 import voogasalad_GucciGames.gameEngine.gameConditions.ConditionHandler;
 import voogasalad_GucciGames.gameEngine.gameConditions.ConditionParams;
@@ -33,7 +34,6 @@ public class MainGameEngine implements GameEngineToGamePlayerInterface {
 	private ATurnDecider myTurnDecider;
 	private ConditionHandler myConditionHandler;
 
-	
 	private ActionToRuleManager myRuleManager;
 	private int mapDimensions;
 	private String myName;
@@ -55,8 +55,6 @@ public class MainGameEngine implements GameEngineToGamePlayerInterface {
 
 		return myName;
 	}
-
-
 
 	@Override
 	public void endTurn() {
@@ -93,70 +91,71 @@ public class MainGameEngine implements GameEngineToGamePlayerInterface {
 		return ((MapObject) myMapObject).performRequest(action, new BasicParameters(this, ((MapObject) myMapObject)));
 
 	}
-	
-	
 
-////////
+	////////
 	public void createTestCondition() {
-			List<Integer> pl = new ArrayList<Integer>();
-			pl.add(0);
-			ConditionParams condParams = new ConditionParams("PlayerUnitCondition", "player", pl, null);
-			ConditionsFactory factory = new ConditionsFactory();
-			BasicParameters comParams = new BasicParameters(myGamePlayers, null, null);
+		List<Integer> pl = new ArrayList<Integer>();
+		pl.add(0);
+		ConditionParams condParams = new ConditionParams("PlayerUnitCondition", "player", pl, null);
+		ConditionsFactory factory = new ConditionsFactory();
+		BasicParameters comParams = new BasicParameters(myGamePlayers, null, null);
 
-			try {
-				Conditions condition = factory.createCondition(condParams, comParams);
-				myConditionHandler.addCondition("PlayerUnitCondition", condition);
-			} catch (InstantiationException | IllegalAccessException | ClassNotFoundException | IllegalArgumentException
-					| InvocationTargetException | NoSuchMethodException | SecurityException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			endTurn();
-			endTurn();
+		try {
+			Conditions condition = factory.createCondition(condParams, comParams);
+			myConditionHandler.addCondition("PlayerUnitCondition", condition);
+		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException | IllegalArgumentException
+				| InvocationTargetException | NoSuchMethodException | SecurityException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		endTurn();
+		endTurn();
+	}
+
+	public void testRules() {
+		System.out.println("create rules");
+		RuleFactory factory = new RuleFactory();
+		RuleParams params = new RuleParams("move", null, null);
+		ActionToRuleManager manager = new ActionToRuleManager();
+		BasicParameters comParams = new BasicParameters(myGamePlayers, null, manager);
+		try {
+			factory.createRule(params, comParams);
+		} catch (NoSuchMethodException | SecurityException | ClassNotFoundException | InstantiationException
+				| IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			e.printStackTrace();
 		}
 
-		public void testRules() {
-			System.out.println("create rules");
-			RuleFactory factory = new RuleFactory();
-			RuleParams params = new RuleParams("move", null, null);
-			ActionToRuleManager manager = new ActionToRuleManager();
-			BasicParameters comParams = new BasicParameters(myGamePlayers, null, manager);
-			try {
-				factory.createRule(params, comParams);
-			} catch (NoSuchMethodException | SecurityException | ClassNotFoundException | InstantiationException
-					| IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-				e.printStackTrace();
-			}
-
-			//execute rule:
-			System.out.println("test rules");
-			List<Rules> rules=manager.getRulesForAction("move");
-			for(int i = 0 ; i < rules.size();i++){
-				rules.get(i).executeRules(comParams, 0);
-			}
-
+		// execute rule:
+		System.out.println("test rules");
+		List<Rules> rules = manager.getRulesForAction("move");
+		for (int i = 0; i < rules.size(); i++) {
+			rules.get(i).executeRules(comParams, 0);
 		}
 
-		public AllPlayers getPlayers() {
-			// TODO Auto-generated method stub
-			return myGamePlayers;
-		}
+	}
 
-		public ActionToRuleManager getActionToRuleManager() {
-			// TODO Auto-generated method stub
-			return this.myRuleManager;
-		}
-		
-		public int getMapDimensions(){
-			return this.mapDimensions;
-		}
+	public AllPlayers getPlayers() {
+		// TODO Auto-generated method stub
+		return myGamePlayers;
+	}
 
-		@Override
-		public ActionToGamePlayerParameters performAction(String action, PlayerMapObjectInterface mapObject,
-				ATargetCoordinate target) {
-			// TODO Auto-generated method stub
-			return null;//((MapObject) mapObject).performAction(action, );
-		}
+	public ActionToRuleManager getActionToRuleManager() {
+		// TODO Auto-generated method stub
+		return this.myRuleManager;
+	}
+
+	public int getMapDimensions() {
+		return this.mapDimensions;
+	}
+
+	@Override
+	public ActionToGamePlayerParameters performAction(String action, PlayerMapObjectInterface mapObject,
+			ATargetCoordinate target) {
+		// TODO Auto-generated method stub
+		return ((MapObject) mapObject).performAction(action,
+				new LocationParams(new BasicParameters(this, ((MapObject) mapObject)),
+						target.getListOfCoordinates().get(0),
+						this.getPlayers().getPlayerById(((MapObject) mapObject).getPlayerID()).getMyMovable()));
+	}
 }
