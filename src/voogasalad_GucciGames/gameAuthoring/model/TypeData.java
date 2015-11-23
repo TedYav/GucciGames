7ebcf8d2115a4a77b3541abe037fa5d159196ed0
Daniel McKee <daniel.mckee@duke.edu>
2015-11-23 01:02:27 -0500

    today is a good day to have a good day

--- src/voogasalad_GucciGames/gameAuthoring/model/TypeData.java
+++ src/voogasalad_GucciGames/gameAuthoring/model/TypeData.java
@@ -21,12 +21,12 @@ public class TypeData {
 	TypeData() {
 		tileTypes = FXCollections.observableArrayList();
 		// Hard coded for testing purposes:
-		MapObjectType objType = new MapObjectType("Grass", "player/images/dummytexture.jpg");
-		MapObjectType objType2 = new MapObjectType("Water", "player/images/dummytexture2.jpg");
-		objType.addDefaultCharacteristic("TileCharacteristic", new TileCharacteristic());
-		objType2.addDefaultCharacteristic("TileCharacteristic", new TileCharacteristic());
-		tileTypes.add(objType);
-		tileTypes.add(objType2);
+//		MapObjectType objType = new MapObjectType("Grass", "player/images/dummytexture.jpg");
+//		MapObjectType objType2 = new MapObjectType("Water", "player/images/dummytexture2.jpg");
+//		objType.addDefaultCharacteristic("TileCharacteristic", new TileCharacteristic());
+//		objType2.addDefaultCharacteristic("TileCharacteristic", new TileCharacteristic());
+//		tileTypes.add(objType);
+//		tileTypes.add(objType2);
 		
 		unitTypes = FXCollections.observableArrayList();
 		MapObjectType unitType1 = new MapObjectType("duvall", "player/images/duvall.png");
