a0f22b3f5faadd2b2f3aab2933e5d5e419d0ab33
Mike Ma <ym67@duke.edu>
2015-11-23 23:11:41 -0500

    Fixed a problem with MD5 Hash

--- src/voogasalad_GucciGames/gameData/XStreamGameEngine.java
+++ src/voogasalad_GucciGames/gameData/XStreamGameEngine.java
@@ -1,24 +1,11 @@
 package voogasalad_GucciGames.gameData;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Map;
-import java.util.TreeMap;
 
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.io.xml.DomDriver;
 
 import voogasalad_GucciGames.gameEngine.MainGameEngine;
-import voogasalad_GucciGames.gameEngine.gamePlayer.AllPlayers;
-import voogasalad_GucciGames.gameEngine.gamePlayer.GamePlayerPerson;
-import voogasalad_GucciGames.gameEngine.mapObject.MapObject;
-import voogasalad_GucciGames.gameEngine.mapObject.MapObjectType;
-import voogasalad_GucciGames.gameEngine.targetCoordinate.TargetCoordinateSingle;
 import voogasalad_GucciGames.gameplayer.controller.GameDataInterface;
 import voogasalad_GucciGames.gameplayer.controller.GameEngineToGamePlayerInterface;
 
@@ -42,16 +29,7 @@ public class XStreamGameEngine implements GameDataInterface{
         }
     }
     public void saveEngine(MainGameEngine engine, String filePath) {
-        try {
-            String engineXML = serializer.toXML(engine);
-            File file = new File(filePath);
-            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file));
-            bufferedWriter.write(engineXML);
-            bufferedWriter.close();
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
+        	saveEngine(engine, new File(filePath));
     }
 
     public GameEngineToGamePlayerInterface loadEngine() {
@@ -59,19 +37,20 @@ public class XStreamGameEngine implements GameDataInterface{
     }
 
     public GameEngineToGamePlayerInterface loadEngine(String path) {
-        String myPath = path;
         if (path.isEmpty()) {
-            myPath=defaultEngineLocation;
+            path=defaultEngineLocation;
+        }
+        return loadEngine(new File(path));
+    }
+    
+    public GameEngineToGamePlayerInterface loadEngine(File file) {
+        if (file==null || !file.canRead()) {
+        	file = new File(defaultEngineLocation);
         }
         System.out.println("Loading engine.");
         MainGameEngine engine=null;
         try {
-            File file = new File(myPath);
-            //            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
-            //            StringBuilder engineXMLBuilder = new StringBuilder();
-            //            bufferedReader.lines().forEach(line->engineXMLBuilder.append(line));
             String engineXML = myLoader.read(file);
-
             // loaded XML File should have current turn as 2
             engine = (MainGameEngine) serializer.fromXML(engineXML);
         }
