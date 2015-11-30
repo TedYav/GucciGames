32bd71abfdaf38abc40c981f98324fa8cdd6ef76
Daniel McKee <daniel.mckee@duke.edu>
2015-11-29 22:40:07 -0500

    added gameInfo class

---
+++ src/voogasalad_GucciGames/gameData/GameInfo.java
@@ -0,0 +1,26 @@
+package voogasalad_GucciGames.gameData;
+
+import java.util.List;
+
+import voogasalad_GucciGames.gameEngine.MainGameEngine;
+
+/**
+ * 
+ * This class will be a wrapper for the game engine
+ * and the information to configure the game player
+ * gui
+ *
+ */
+
+
+public class GameInfo {
+	private MainGameEngine myEngine;
+	// these will hold the components which go in each part of the player gui
+	// format can be changed
+	private List<String> rightBar;	// hold class names
+	private List<String> leftBar;
+	
+	
+
+}
+
