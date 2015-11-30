f9140f68aac3312df92d18902c535ebe700c5008
Daniel McKee <daniel.mckee@duke.edu>
2015-11-30 01:18:14 -0500

    Added outlie for top menu bar

--- src/voogasalad_GucciGames/gameplayer/windows/mainwindow/menubar/MenuLoader.java
+++ src/voogasalad_GucciGames/gameplayer/windows/mainwindow/menubar/MenuLoader.java
@@ -13,8 +13,10 @@ import voogasalad_GucciGames.gameplayer.controller.GameControllerInterface;
 
 public class MenuLoader {
 	private static final String PREFIX = MenuLoader.class.getPackage().getName();
-	private static final String MENU_PATH = PREFIX + ".menubar";
-	private static final String LIST_PATH = PREFIX + ".menulists";
+	private static final String MENU_PATH = PREFIX + ".MenuItems";
+	private static final String LIST_PATH = PREFIX + ".MenuLists";
+	private static final String NAMES_PATH = PREFIX + ".ItemNames";
+
 
 	
 	public List<Menu> load(GameControllerInterface controller){		
@@ -22,6 +24,7 @@ public class MenuLoader {
 		Map<String, Menu> map = new HashMap<>();
 		ResourceBundle listBundle = ResourceBundle.getBundle(LIST_PATH);
 		ResourceBundle menuBundle = ResourceBundle.getBundle(MENU_PATH);
+		ResourceBundle namesBundle = ResourceBundle.getBundle(NAMES_PATH);
 		
 		listBundle.keySet().stream().forEach(e -> {
 			nameList.add(e);
@@ -29,20 +32,21 @@ public class MenuLoader {
 		});
 		
 		listBundle.keySet().stream().forEach(dropdownname -> {
-			System.out.println(dropdownname);
 			Arrays.asList(listBundle.getString(dropdownname).split(",")).stream()
 			.forEach(componentName -> {
 				try {
-					map.get(dropdownname).getItems().add(getItem(menuBundle.getString(componentName), componentName, controller));
+					map.get(dropdownname).getItems()
+					.add(getItem(menuBundle.getString(componentName), 
+							namesBundle.getString(componentName), controller));
 				} catch (Exception e1) {
 					// TODO Auto-generated catch block
-					e1.printStackTrace();
+					//e1.printStackTrace();
 				}
 			});
 		});
-		return new ArrayList<>(map.values());
-
-
+		List<Menu> list = new ArrayList<>();
+		nameList.forEach(e -> list.add(map.get(e)));
+		return list;
 	}
 	
 	private MenuItem getItem(String className, String itemName, GameControllerInterface controller) throws Exception {
--- src/voogasalad_GucciGames/gameplayer/windows/mainwindow/menubar/SaveItem.java
+++ src/voogasalad_GucciGames/gameplayer/windows/mainwindow/menubar/SaveItem.java
@@ -4,13 +4,12 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.input.KeyCombination;
 import voogasalad_GucciGames.gameplayer.controller.GameControllerInterface;
 
-public class OpenItem extends MenuItem{
-	OpenItem(String name, GameControllerInterface controller) {
-		super(name);
-		setOnAction(e -> {
-			//controller.saveToXML();
-			//TODO
-		});
+public class SaveItem extends MenuItem{
+		SaveItem(String name, GameControllerInterface controller) {
+			super(name);
+			setOnAction(e -> {
+				//controller.saveToXML();
+				//TODO
+			});
+		}
 	}
-}
-
