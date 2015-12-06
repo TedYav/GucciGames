81eb28c362a3c450056c715007df5edbf9ef9024
Daniel McKee <daniel.mckee@duke.edu>
2015-12-06 04:35:55 -0500

    outline for game info factory

--- src/voogasalad_GucciGames/gameAuthoring/model/TypeData.java
+++ src/voogasalad_GucciGames/gameAuthoring/model/TypeData.java
@@ -132,6 +132,18 @@ public class TypeData implements IGameProperties {
 		
 	}
 
+	@Override
+	public void addAction(ActionParams params, MapObjectType type) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addCharacteristic(ObjParam param, MapObjectType type) {
+		// TODO Auto-generated method stub
+		
+	}
+
 	
 
 
