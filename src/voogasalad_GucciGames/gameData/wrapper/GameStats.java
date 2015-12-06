d6db9e59fed200d05342502c0c71584262bbb3ea
Tina Liang <tina.tang.tl@gmail.com>
2015-12-06 04:02:21 -0500

    fix merge

---
+++ src/voogasalad_GucciGames/gameData/wrapper/GameStats.java
@@ -0,0 +1,13 @@
+package voogasalad_GucciGames.gameData.wrapper;
+
+public class GameStats {
+	
+	public void storeTransferable(){
+		
+	}
+
+	public void initializeLevel(){
+		//should use values stored in Game stats and update 
+	}
+	
+}
