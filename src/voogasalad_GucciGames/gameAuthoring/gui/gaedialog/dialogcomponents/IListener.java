f037ba2d28ca86112186b23dede9632e8b2a67ab
Ying Qi <yq25@duke.edu>
2015-12-13 23:57:00 -0500

    added masterpiece marker

--- src/voogasalad_GucciGames/gameAuthoring/gui/gaedialog/dialogcomponents/IListener.java
+++ src/voogasalad_GucciGames/gameAuthoring/gui/gaedialog/dialogcomponents/IListener.java
@@ -1,3 +1,5 @@
+// This entire file is part of my masterpiece.
+// Ying Qi
 package voogasalad_GucciGames.gameAuthoring.gui.gaedialog.dialogcomponents;
 
 import javafx.scene.control.ToggleGroup;
--- src/voogasalad_GucciGames/gameAuthoring/gui/gaedialog/mapobjsettings/ActionDialog.java
+++ src/voogasalad_GucciGames/gameAuthoring/gui/gaedialog/mapobjsettings/ActionDialog.java
@@ -1,3 +1,5 @@
+// This entire file is part of my masterpiece.
+// Ying Qi
 package voogasalad_GucciGames.gameAuthoring.gui.gaedialog.mapobjsettings;
 
 import javafx.scene.control.ButtonBar.ButtonData;
